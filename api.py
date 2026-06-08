e1cbac91130ba795088e487ef1078b19e4cdae4c
Lucas Stefenoni <107085569+LucasStefenoni@users.noreply.github.com>
2026-06-08 16:40:25 -0300

    Add error handling for film recommendations
    Handle exceptions in the film recommendation process.

--- api.py
+++ api.py
@@ -109,7 +109,7 @@ def ler_pickle(arquivo):
         print(f"Erro: O arquivo {arquivo} não foi encontrado!")
         exit()
 
-model = SentenceTransformer("all-MiniLM-L6-v2")    
+model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")   
 catalogo_fixo = ler_json("catalogo_fixo.json")
 ids_salvos = {filme['id'] for filme in catalogo_fixo}
 embeddings_fixos = ler_pickle("embeddings_fixos.pkl")
